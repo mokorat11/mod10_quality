c068da780ff72a4271bb80e984f4a821b7cd66c2
mokorat11 <amogh-koratpallikar@uiowa.edu>
2025-04-10 16:24:35 -0500

    changes

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 """This is a simple Flask application."""
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     """Return a greeting message"""
-    return "Hello, World!"
+    return render_template("index.htlml")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
